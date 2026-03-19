b3284b330fbf43460eb5bd496944d1f4fabfacab
Binuka Methvin <105734702+Aerota@users.noreply.github.com>
2026-03-19 06:45:51 +0530

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -13,23 +13,18 @@ export default function DashboardLayout({
 
   return (
     <div className="min-h-screen bg-gray-50 flex">
-      {/* Mobile sidebar overlay */}
       {sidebarOpen && (
         <div
           className="fixed inset-0 bg-black bg-opacity-50 z-30 lg:hidden"
           onClick={() => setSidebarOpen(false)}
         />
       )}
-
-      {/* Sidebar */}
       <div className={`
         fixed inset-y-0 left-0 transform ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'}
         lg:relative lg:translate-x-0 transition duration-200 ease-in-out z-40 lg:z-0
       `}>
         <Sidebar />
       </div>
-
-      {/* Main content */}
       <div className="flex-1 flex flex-col min-h-screen">
         <Header setSidebarOpen={setSidebarOpen} />
         <main className="flex-1">
